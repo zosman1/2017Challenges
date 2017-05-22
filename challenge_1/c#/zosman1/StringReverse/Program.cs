8478770bf00d3a75ca5fec374ba53adfb0827f22
Zach Osman <zosman@gmu.edu>
2017-05-21 23:47:13 -0400

    Completed Challange#1 in C#

---
+++ challenge_1/c#/zosman1/StringReverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringReverse
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("Hello!");
+            Console.WriteLine("Please enter a string you would like reversed!");
+            string input = Console.ReadLine();
+            string output = "";
+            foreach(char character in input){
+                if (output != null){
+                    output = character.ToString() + output;
+				}
+                else {
+                    output = character.ToString();
+                }
+            }
+            Console.WriteLine("Reversed String: " + output);
+		}
+    }
+}
